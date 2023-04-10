493adc8917725cf04a92a7658b00af93dcc98010
Tebanqito <yemesesteban@gmail.com>
2023-04-10 15:27:27 -0300

    ejercicio 1 hecho

--- clases/homeworks/ejercicio 1.ts
+++ clases/homeworks/ejercicio 1.ts
@@ -10,15 +10,15 @@ let resultado = variableExplicita + variableInferida
 
 //completar el tipado correspondiente para cada variable
 
-let unString: null = 'mati';
+let unString: string = 'mati';
 
-let unNumber: null = 2;
+let unNumber: number = 2;
 
-let unBoolean: null = true;
+let unBoolean: boolean = true;
 
-let unNull: undefined = null; //que pasa aca?
+let unNull: null = null; //que pasa aca?
 
-let unUndefined: null = undefined //y aca?
+let unUndefined: undefined = undefined //y aca?
 
 	
 // Enum
@@ -36,3 +36,7 @@ enum userActions {
 
 
 export {}
+
+// para ejecutar el codigo de typescritp: npx tsc 
+// para compilar: npx tsc nombreArchivo.ts
+// para crar nuestro archivo de configuracion: npx tsc --init
