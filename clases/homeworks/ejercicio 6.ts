6db6d0ef5dd99f0c307b5a7d51cfdc6978b2f14a
Tebanqito <yemesesteban@gmail.com>
2023-04-11 15:46:26 -0300

    ejercicio 6 hecho

--- clases/homeworks/ejercicio 6.ts
+++ clases/homeworks/ejercicio 6.ts
@@ -1,3 +1,5 @@
-function suma(a: number, b: number): number {
-    return a + b
+function sum(a: number | string, b: number | string): number {
+    if (typeof a === "string") a = parseInt(a);
+    if (typeof b === "string") b = parseInt(b);
+    return a + b;
 } //que pasa si yo quisiera concatenar strings con esta funcion?
