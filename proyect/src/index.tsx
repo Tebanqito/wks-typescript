aba1f7963300dbf1d3a5bc9076157f119caebd4b
Tebanqito <yemesesteban@gmail.com>
2023-04-17 19:02:27 -0300

    prueba

--- proyect/src/index.tsx
+++ proyect/src/index.tsx
@@ -1,19 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+interface AppProps {
+  title: string;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = ({ title }: AppProps): JSX.Element => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h3>Henry Workshop - {title}</h3>
+      <hr></hr>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <br></br>
+      <span>{counter}</span>
+    </div>
+  );
+}
+
+// utilizando class
+
+// interface AppState {
+//   counter: number;
+// }
+
+// class App extends React.Component<AppProps, AppState> {
+//   constructor(props: AppProps) {
+//     super(props);
+//     this.state = {counter: 0};
+// }
+ 
+//   onIncrement = (): void => {
+//    this.setState({counter: this.state.counter + 1});
+//   };
+ 
+//   onDecrement = (): void => {
+//    this.setState({counter: this.state.counter - 1});
+//   };
+ 
+//   render() {
+//    return (
+//     <div>
+//      <h3>Henry Workshop - {this.props.title}</h3>
+//      <hr></hr>
+//      <button onClick={this.onIncrement}>Increment</button>
+//      <button onClick={this.onDecrement}>Decrement</button>
+//      <br></br>
+//      <span>{this.state.counter}</span>
+//     </div>
+//    );
+//   }
+//  }
+
+ReactDOM.render(<App title="Typescript" />, document.querySelector('#root'));
